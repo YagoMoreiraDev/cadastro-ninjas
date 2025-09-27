fefbeb02b480a1253461c879f446ac98e43856a4
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-09-27 17:55:42 -0300

    Criado projeto Cadastro de Ninjas - Java 10x

---
+++ src/test/java/com/cloud/yagodev/cadastroninja/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.yagodev.cadastroninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
